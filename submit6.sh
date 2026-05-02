76a9f5997d87dcab4caf53a6cfa0e773987eb6a1
Madsemandd <127887836+Madsemandd@users.noreply.github.com>
2026-05-02 21:53:01 +0200

    Add files via upload

---
+++ submit6.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#BSUB -J python
+#BSUB -q hpc
+#BSUB -W 25
+#BSUB -R "rusage[mem=512MB]"
+#BSUB -n 20
+#BSUB -R "span[hosts=1]"
+#BSUB -o python_%J.out
+#BSUB -e python_%J.err
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613_2026
+
+
+python task6.py 20
